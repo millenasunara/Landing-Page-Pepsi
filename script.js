c0553a3aaa81aded6461455fc656b62a64602962
millenasunara <millena.sunara.ms@gmail.com>
2024-03-13 13:42:37 -0300

    landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// =-=-=-=-=-= IMAGE SLIDER =-=-=-=-=-= //
+function imgSlider(anything){
+    document.querySelector(".pepsi").src = anything;
+}
+
+// =-=-=-=-=-= CHANGE BACKGROUND COLOR =-=-=-=-=-= //
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
